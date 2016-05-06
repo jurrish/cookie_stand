099247cee43b0ca70d0582b1f2e170229c9c5b9a
Frank Iriarte <jurrish@gmail.com>
2016-05-05 17:17:43 -0700

    trying to get my form and event handlers dialed in

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// var elPikePlace = document.getElementById('store_data');
-
 var timeArray = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+var cookieStores = [];//ASSIGNING empty array to the variable "stores"
 
 function Store(name, minCustomers, maxCustomers, averageCookiePerSale) {
   this.name = name;
@@ -9,19 +8,19 @@ function Store(name, minCustomers, maxCustomers, averageCookiePerSale) {
   this.averageCookiePerSale = averageCookiePerSale;
   this.cookiesPerHour = [];
   this.totalSales = 0;
+  cookieStores.push(this);//when we're instantiating a new object, we're pushing that instance into the store array.
 }
 
-Store.prototype.personPerHour = function(min, max) {//creating a method to calculate a random amount of guests that will buy cookies
-  return Math.random() * (max - min + 1) + min;
+Store.prototype.personPerHour = function(min, max) {
+  return Math.random() * (max - min) + min;
 };
 
-Store.prototype.cookieSalePerHour = function() {//create a method to calculate a random amount of cookies sold per hour based on personPerHour random calculation
+Store.prototype.cookieSalePerHour = function() {//all instances will inherit the method cookieSalePerHour
   for(var i = 0; i < timeArray.length; i++) {
     var cookies = Math.floor(this.averageCookiePerSale * this.personPerHour(this.minCustomers, this.maxCustomers));
     this.cookiesPerHour.push(cookies);
-    console.log(this.cookiesPerHour);
     this.totalSales += cookies;
-  };
+  }
 };
 
 Store.prototype.render = function() {
@@ -29,28 +28,139 @@ Store.prototype.render = function() {
 
   var ulEl = document.createElement('ul');
   ulEl.appendChild(document.createTextNode(this.name));
-  var sectionEl = document.getElementById('store_data').appendChild(ulEl);//we are assigning an element to a variable, also appending the ul to the section of store data, but just the section is being assigned a value.
+  document.getElementById('store_data').appendChild(ulEl);
 
-  for (var j = 0; j < timeArray.length; j++) {
+  for(var j = 0; j < timeArray.length; j++) {
     var liEl = document.createElement('li');
-    console.log('timeArray: ' + timeArray[j] + ' cookiesPerHour[j] : ' + this.cookiesPerHour[j]);
-    liEl.textContent = timeArray[j] + ': ' + this.cookiesPerHour[j];
+    liEl.textContent = 'Hourly Sale@ ' + ' ' + timeArray[j] + ' : ' + this.cookiesPerHour[j];
     ulEl.appendChild(liEl);
   }
+  var totalEl = document.createElement('li');
+  totalEl.textContent = 'Total Daily Sales: ' + this.totalSales;
+  ulEl.appendChild(totalEl);
+};
+var tableEl = document.getElementById('stores');
 
-  var liEl_two = document.createElement('li');
-  liEl_two.textContent = 'Total: ' + this.totalSales;
-  ulEl.appendChild(liEl_two);
+Store.render = function() {
+  // var tableEl = document.getElementById('stores');
+  var hoursRow = document.createElement('tr');
+  var emptyCell = document.createElement('th');
+  hoursRow.appendChild(emptyCell);
+  for (var i = 0; i < timeArray; i++) {
+    tableHeader.textContent = timeArray[i];
+    hoursRow.appendChild(tableHeader);
+  }
+  var totalHeader = document.createElement('th');
+  totalHeader.textContent = 'Total';
+  hoursRow.appendChild(totalHeader);
+  tableEl.appendChild(hoursRow);
+
+  for(obj in cookieStores) { //for each object in cookiestores, object will represent the index (0, 1 ,2 ,3 ,4,5) in hours open
+    cookieStores[obj].cookieSalePerHour();//gets object you're iterating over
+    var tableRow = document.createElement('tr');//create new row
+    var nameRow = document.createElement('td');
+    nameRow.textContent = cookieStores[obj].name;//adds a name to the td in the nameRow
+    tableRow.appendChild(nameRow);
+
+    for (hour in timeArray) {
+      var tableData = document.createElement('td');
+      tableData.textContent = cookieStores[obj].cookiesPerHour[hour];//getting hourlySales at 10,11,12,1,2, while the cookieStore stays on the pikeplace object.
+      tableRow.appendChild(tableData);
+    }
+    var totalsData = document.createElement('td');
+    totalsData.textContent = cookieStores[obj].totalSales;
+    tableRow.appendChild(totalsData);
+    tableEl.appendChild(tableRow);//appending to original tr
+  }
 };
 
-var pikeStore = new Store('Pike Place', 17, 88, 5.2);//why an array at index 4?
+var pikeStore = new Store('Pike Place', 17, 88, 5.2);
 var seaTacStore = new Store('SeaTac', 6, 24, 1.2);
 var southCenter = new Store('Southcenter', 11, 38, 1.9);
 var belleSquare = new Store('Bellevue Square', 20, 48, 3.3);
 var alki = new Store('Alki', 3, 24, 2.6);
 
-pikeStore.render();
-seaTacStore.render();
-southCenter.render();
-belleSquare.render();
-alki.render();
+Store.render();
+
+Store.renderNew = function(obj) {
+  var formRow = document.createElement('tr');
+  var formNameTd = document.createElement('td');
+  formNameTd.textContent = obj.name;
+  formRow.appendChild(formNameTd);
+  for(hour in timeArray) {
+    var salesTd = document.createElement('td');
+    salesTd.textContent = obj.cookieSalePerHour[hour];
+    formNameTd.appendChild(salesTd);
+  }
+  var newTotalTd = document.createElement('td');
+  newTotalTd.textContent = obj.totalSales;
+  formRow.appendChild(newTotalTd);
+  tableEl.appendChild(formRow);
+};
+
+var formEl = document.getElementById('form');
+
+formEl.addEventListner('submit', function(event) {
+  event.preventDefault();
+  var newStoreName = event.target.newstorelocation.value;//newstorelocation is in html
+  var newMinCust = parseInt(event.target.min.value);//id = min
+  var newMaxCust = parseInt(event.target.max.value);//id = max
+  var newAvgCustSale = parseFloat(event.target.avg.value);
+  var newShop = new Store(newStoreName, newMinCust, newMaxCust, newAvgCustSale);
+  newShop.cookieSalePerHour();
+  Store.renderNew(newShop);
+});
+
+// function Store(name, minCustomers, maxCustomers, averageCookiePerSale) {
+//   this.name = name;
+//   this.minCustomers = minCustomers;
+//   this.maxCustomers = maxCustomers;
+//   this.averageCookiePerSale = averageCookiePerSale;
+//   this.cookiesPerHour = [];
+//   this.totalSales = 0;
+//   storesAvailable.push(this);
+// }
+//
+// Store.prototype.personPerHour = function(min, max) {//creating a method to calculate a random amount of guests that will buy cookies
+//   return Math.random() * (max - min + 1) + min;
+// };
+//
+// Store.prototype.cookieSalePerHour = function() {//create a method to calculate a random amount of cookies sold per hour based on personPerHour random calculation
+//   for(var i = 0; i < timeArray.length; i++) {//iterate over the timeArray
+//     var cookies = Math.floor(this.averageCookiePerSale * this.personPerHour(this.minCustomers, this.maxCustomers));//this determines the amount of cookies sold per hour based on average cookie sold per hour per store multiplied by the average guest count per hour and deducts the decimal place with math.floor
+//     this.cookiesPerHour.push(cookies);//cookies is being pushed into the cookies per hour array
+//     console.log(this.cookiesPerHour);
+//     this.totalSales += cookies;//this.totalSales = this.totalSales + cookies
+//   };
+// };
+//
+// Store.prototype.render = function() {
+//   this.cookieSalePerHour();
+//
+//   var ulEl = document.createElement('ul');
+//   ulEl.appendChild(document.createTextNode(this.name));
+//   var sectionEl = document.getElementById('store_data').appendChild(ulEl);//we are assigning an element to a variable, also appending the ul to the section of store data, but just the section is being assigned a value.
+//
+//   for (var j = 0; j < timeArray.length; j++) {
+//     var liEl = document.createElement('li');
+//     console.log('timeArray: ' + timeArray[j] + ' cookiesPerHour[j] : ' + this.cookiesPerHour[j]);
+//     liEl.textContent = timeArray[j] + ': ' + this.cookiesPerHour[j];
+//     ulEl.appendChild(liEl);
+//   }
+//
+//   var liEl_two = document.createElement('li');
+//   liEl_two.textContent = 'Total: ' + this.totalSales;
+//   ulEl.appendChild(liEl_two);
+// };
+//
+// var pikeStore = new Store('Pike Place', 17, 88, 5.2);//why an array at index 4?
+// var seaTacStore = new Store('SeaTac', 6, 24, 1.2);
+// var southCenter = new Store('Southcenter', 11, 38, 1.9);
+// var belleSquare = new Store('Bellevue Square', 20, 48, 3.3);
+// var alki = new Store('Alki', 3, 24, 2.6);
+//
+// pikeStore.render();
+// seaTacStore.render();
+// southCenter.render();
+// belleSquare.render();
+// alki.render();
